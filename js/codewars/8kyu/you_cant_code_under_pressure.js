e05a2bf771b600b60bb6508514d56b123693da7d
jp <johnamata@protonmail.com>
2022-06-15 02:29:09 +0800

    grind

--- js/codewars/8kyu/you_cant_code_under_pressure.js
+++ js/codewars/8kyu/you_cant_code_under_pressure.js
@@ -1,6 +1,9 @@
 //https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
 //double the integer and return it.
-
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i * 2;
+}
 //tests
 /*
 describe("Tests", () => {
@@ -9,3 +12,17 @@ describe("Tests", () => {
     });
   });
 */
+
+//other people's solutions, things to learn from
+//=============================================================
+//double with +
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i + i;
+}
+//another alternative
+const doubleInteger = (i) => i * 2;
+//this guy uses bit shift
+function doubleInteger(i) {
+  return i << 1;
+}
