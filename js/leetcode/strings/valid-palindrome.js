6ce56c2473f8f56cf0a432098c4c1e2a3631c037
jp <johnamata@protonmail.com>
2022-07-03 00:01:28 +0800

    grind

--- js/leetcode/strings/valid-palindrome.js
+++ js/leetcode/strings/valid-palindrome.js
@@ -1,4 +1,13 @@
 //https://leetcode.com/problems/valid-palindrome/
+/*
+ s = s.toLowerCase().replace(/[\W_:]/g,"");
+ time - O(n) 
+ space - O(1), just the pointers
+ APPROACH: Two pointer
+ kind of like th 2 pointer approahc for reverse string except we dont do the swap, we just compare.
+
+ also since it's comparison, cleaning the string to make them easy to compare with (removing case sensitivity and other characters like "_" underscores and more) is a must
+ */
 /**
  * @param {string} s
  * @return {boolean}
