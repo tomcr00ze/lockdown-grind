87874c0048b26dbd2eba7d4201d3c17a6ab26338
jp <johnamata@protonmail.com>
2022-07-02 23:51:30 +0800

    grind

--- js/leetcode/strings/valid-palindrome.js
+++ js/leetcode/strings/valid-palindrome.js
@@ -3,4 +3,17 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function (s) {};
+var isPalindrome = function (s) {
+  s = s.toLowerCase().replace(/[\W_:]/g, '');
+  let l = 0;
+  let r = s.length - 1;
+  while (l < r) {
+    if (s[l] == s[r]) {
+      l++;
+      r--;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
